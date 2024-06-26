178fb073aa16e23bf7079256accc962a37299753
ochi <syauqialfeth@gmail.com>
2024-06-26 14:10:15 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,265 @@
+let WALLET_CONNECTED = "";
+let contractAddress = "0x461887201A268D55A0e36C7a97640C338612Eab1";
+let contractAbi = [
+    {
+      "inputs": [
+        {
+          "internalType": "string[]",
+          "name": "_candidateNames",
+          "type": "string[]"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_durationInMinutes",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        }
+      ],
+      "name": "addCandidate",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "candidates",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "voteCount",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getAllVotesOfCandiates",
+      "outputs": [
+        {
+          "components": [
+            {
+              "internalType": "string",
+              "name": "name",
+              "type": "string"
+            },
+            {
+              "internalType": "uint256",
+              "name": "voteCount",
+              "type": "uint256"
+            }
+          ],
+          "internalType": "struct Voting.Candidate[]",
+          "name": "",
+          "type": "tuple[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getRemainingTime",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_candidateIndex",
+          "type": "uint256"
+        }
+      ],
+      "name": "getVotesOfCandiate",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "getVotingStatus",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_candidateIndex",
+          "type": "uint256"
+        }
+      ],
+      "name": "vote",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "voters",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "votingEnd",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "votingStart",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
+
+const connectMetamask = async() => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    const signer = provider.getSigner();
+    WALLET_CONNECTED = await signer.getAddress();
+    var element = document.getElementById("metamasknotification");
+    element.innerHTML = "Metamask telah terhubung " + WALLET_CONNECTED;
+}
+
+const addVote = async() => {
+    if(WALLET_CONNECTED != 0) {
+        var name = document.getElementById("vote");
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const contractInstance = new ethers.Contract(contractAddress, contractAbi, signer);
+        var cand = document.getElementById("cand");
+        cand.innerHTML = "Tunggu sebentar, menambahkan vote ke Smart Contract";
+        const tx = await contractInstance.vote(name.value);
+        await tx.wait();
+        cand.innerHTML = "Terimakasih telah menggunakan hak suara Anda";
+    }
+    else {
+        var cand = document.getElementById("cand");
+        cand.innerHTML = "Hubungkan dengan Metamask terlebih dahulu !";
+    }
+}
+
+const voteStatus = async() => {
+    if(WALLET_CONNECTED != 0) {
+        var status = document.getElementById("status");
+        var remainingTime = document.getElementById("time");
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const contractInstance = new ethers.Contract(contractAddress, contractAbi, signer);
+        const currentStatus = await contractInstance.getVotingStatus();
+        const time = await contractInstance.getRemainingTime();
+        console.log(time);
+        status.innerHTML = currentStatus == 1 ? "Pemilihan sudah dibuka" : "Pemilihan sudah berakhir";
+        remainingTime.innerHTML = `Waktu pemilihan tersisa ${parseInt(time, 16)} detik`;
+    }
+    else {
+        var status = document.getElementById("status");
+        status.innerHTML = "Hubungkan dengan Metamask terlebih dahulu !";
+    }
+}
+
+const getAllCandidates = async() => {
+    if(WALLET_CONNECTED != 0) {
+        var p3 = document.getElementById("p3");
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const contractInstance = new ethers.Contract(contractAddress, contractAbi, signer);
+        p3.innerHTML = "Please wait, getting all the candidates from the voting smart contract";
+        var candidates = await contractInstance.getAllVotesOfCandiates();
+        console.log(candidates);
+        var table = document.getElementById("myTable");
+
+        for (let i = 0; i < candidates.length; i++) {
+            var row = table.insertRow();
+            var idCell = row.insertCell();
+            var nameCell = row.insertCell();            
+            var statusCell = row.insertCell();
+
+            idCell.innerHTML = i+1;
+            nameCell.innerHTML = candidates[i].name;        
+            statusCell.innerHTML = candidates[i].voteCount;
+
+        }
+
+        p3.innerHTML = "Berikut Daftar Pasangan Calon"
+    }
+    else {
+        var p3 = document.getElementById("p3");
+        p3.innerHTML = "Hubungkan dengan Metamask terlebih dahulu !";
+    }
+}
